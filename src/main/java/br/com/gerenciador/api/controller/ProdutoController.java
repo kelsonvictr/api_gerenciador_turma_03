1db8e3669c468bfdb004476a578693aa90110b29
kelsonalmeida <kelson.victr@gmail.com>
2025-03-22 11:28:23 -0300

    Aula 13 - Produto até service

---
+++ src/main/java/br/com/gerenciador/api/controller/ProdutoController.java
@@ -0,0 +1,4 @@
+package br.com.gerenciador.api.controller;
+
+public class ProdutoController {
+}
---
+++ src/main/java/br/com/gerenciador/api/service/ProdutoService.java
@@ -0,0 +1,8 @@
+package br.com.gerenciador.api.service;
+
+import br.com.gerenciador.api.dto.ProdutoRequestDTO;
+import br.com.gerenciador.api.dto.ProdutoResponseDTO;
+
+public interface ProdutoService {
+    ProdutoResponseDTO criarProduto(ProdutoRequestDTO dto);
+}
